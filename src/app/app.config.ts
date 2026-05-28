94433e50742c4e204445f4d1132498a42ddf366e
Karthik Vishal <karthikvishal1506@gmail.com>
2026-05-28 09:25:40 +0530

    Added HTTP service Provider in config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http'; // ADD THIS IMPORT
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+    provideBrowserGlobalErrorListeners(),provideHttpClient(), // ADD THIS LINE,
     provideRouter(routes), provideClientHydration(withEventReplay())
   ]
 };
+
