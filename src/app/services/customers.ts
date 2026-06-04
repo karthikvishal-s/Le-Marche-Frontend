218475a4320b8dcdbd2216e928ba959d9978c5d5
Karthik Vishal <karthikvishal1506@gmail.com>
2026-06-04 16:02:17 +0530

    Get api call linked in frontend

--- src/app/services/customers.ts
+++ src/app/services/customers.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
 export interface Customer{
@@ -13,6 +15,14 @@ export interface Customer{
   providedIn: 'root',
 })
 export class Customers {
+
+  private apiurl="http://localhost:8080/customers";
+
+  private http = inject(HttpClient);
+
+  public getCustomers():Observable<Customer[]>{
+      return this.http.get <Customer[]>(this.apiurl);
+  }
 
 
 }
