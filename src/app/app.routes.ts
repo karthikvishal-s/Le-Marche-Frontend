8fcefb76a5c2e2e5787a179acee1b7d06492345a
Karthik Vishal <karthikvishal1506@gmail.com>
2026-06-05 00:03:19 +0530

    Routing made in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './product/product';
+import { CustomerComponent } from './customer/customer';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {path:'product', component:ProductComponent},
+    {path:'customer',component:CustomerComponent},
+   
+];
